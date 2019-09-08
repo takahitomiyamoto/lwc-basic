bc8907c8ed1631c9fe9a22f3b2e9bd88a786e565
takahito.miyamoto <takahito.miyamoto@accenture.com>
2019-09-08 14:12:37 +0000

    update

---
+++ force-app/main/beginner-level-2/lwc/myFirstWebComponent/myFirstWebComponent.js
@@ -0,0 +1,22 @@
+import {LightningElement, track} from 'lwc';
+
+export default class HelloIteration extends LightningElement {
+  @track
+  contacts = [
+    {
+      Id: 1,
+      Name: 'Amy Taylor',
+      Title: 'VP of Engineering'
+    },
+    {
+      Id: 2,
+      Name: 'Michael Jones',
+      Title: 'VP of Sales'
+    },
+    {
+      Id: 3,
+      Name: 'Jennifer Wu',
+      Title: 'CEO'
+    }
+  ];
+}
